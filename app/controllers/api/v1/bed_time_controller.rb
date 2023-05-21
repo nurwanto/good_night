d0ae003e28f0da88b4d49ca7a828172f7630793b
nurwanto-fazz <nurwanto@fazzfinancial.com>
2023-05-21 13:47:47 +0700

    add validation

--- app/controllers/api/v1/bed_time_controller.rb
+++ app/controllers/api/v1/bed_time_controller.rb
@@ -2,16 +2,28 @@ module Api
   module V1
     class BedTimeController < ApplicationController
       def history
-        # TODO: add params validation, error handling if data empty
-
         # current_user variable is to simulate authentication and detect the account
+        if params[:current_user_id].blank?
+          return render json: { error_message: 'current_user should be exist' },
+                        status: :bad_request
+        end
+
         current_user = User.find(params[:current_user_id])
+        if current_user.blank?
+          return render json: { error_message: "user_id #{params[:current_user_id]} not exist" },
+                        status: :not_found
+        end
 
         followers = if params[:user_id].present?
                       current_user.followers.where(id: params[:user_id])
                     else
                       current_user.followers
                     end
+
+        if followers.blank?
+          return render json: { error_message: 'empty followers' },
+                        status: :not_found
+        end
 
         data = []
         followers.each do |follower|
