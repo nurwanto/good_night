5e81c9b34085e59e627232830a961eab6efc9bd7
nurwanto-fazz <nurwanto@fazzfinancial.com>
2023-05-29 16:52:25 +0700

    refactor bed time controller

--- app/controllers/api/v1/bed_time_controller.rb
+++ app/controllers/api/v1/bed_time_controller.rb
@@ -1,82 +1,51 @@
 module Api
   module V1
     class BedTimeController < ApplicationController
+      include Api::V1::BedTimeHelper
       protect_from_forgery with: :null_session
 
+      rescue_from ArgumentError do |e|
+        render json: { error_message: e.message },
+               status: :not_found
+      end
+
+      rescue_from StandardError do |e|
+        render json: { error_message: e.message },
+               status: :bad_request
+      end
+
       def history
-        # current_user variable is to simulate authentication and detect the account
-        if params[:current_user_id].blank?
-          return render json: { error_message: 'current_user_id should be exist' },
-                        status: :bad_request
-        end
+        authenticate!
 
-        current_user = User.find_by(id: params[:current_user_id])
-        if current_user.blank?
-          return render json: { error_message: "user_id #{params[:current_user_id]} not exist" },
-                        status: :not_found
-        end
+        sql_query = "SELECT bth.id, bth.user_id, bth.bed_time, bth.wake_up_time, ROUND((JULIANDAY(bth.wake_up_time) - JULIANDAY(bth.bed_time)) * 86400) as duration FROM user_followers uf INNER JOIN bed_time_histories bth ON bth.user_id = uf.followed_id WHERE bth.created_at >= \"#{10.week.ago}\" AND uf.follower_id = #{@current_user.id} ORDER BY duration DESC"
+        data = ActiveRecord::Base.connection.execute(sql_query)
 
-        followed = if params[:user_id].present?
-                     current_user.followed.where(id: params[:user_id])
-                   else
-                     current_user.followed
-                   end
-
-        if followed.blank?
-          return render json: { error_message: 'You have not followed anyone yet' },
-                        status: :not_found
-        end
-
-        data = []
-        followed.each do |follower|
-          bed_time_histories = follower.bed_time_histories.where('created_at >= ?', 1.week.ago).map do |x|
-            { bed_time: x.bed_time, wake_up_time: x.wake_up_time, duration: (x.wake_up_time - x.bed_time) }
-          end
-
-          bed_time_histories = bed_time_histories.sort_by { |x| x[:duration] }
-
-          data << {
-            user_id: follower.id,
-            user_name: follower.name,
-            bed_time_histories: bed_time_histories
-          }
-        end
-
-        render json: { data: data }
+        render json: { data: data&.map do |x|
+                               { id: x['id'], user_id: x['user_id'], bed_time: x['bed_time'],
+                                 wake_up_time: x['wake_up_time'], duration: x['duration'] }
+                             end }
       end
 
       def set_unset
-        if params[:current_user_id].blank?
-          return render json: { error_message: 'current_user_id should be exist' },
-                        status: :bad_request
-        end
-
-        current_user = User.find_by(id: params[:current_user_id])
-        if current_user.blank?
-          return render json: { error_message: "user_id #{params[:current_user_id]} not exist" },
-                        status: :not_found
-        end
+        authenticate!
 
         system_time = Time.now
         case params[:type].to_s
         when 'bed_time'
-          if current_user.bed_time_histories&.last&.wake_up_time.present?
-            BedTimeHistory.create!(bed_time: system_time, user_id: current_user.id)
-          else
-            return render json: { error_message: "you haven't woken up yet" },
-                          status: :bad_request
+          unless @current_user.bed_time_histories&.last&.wake_up_time.present?
+            raise StandardError, "you haven't woken up yet"
           end
+
+          BedTimeHistory.create!(bed_time: system_time, user_id: @current_user.id)
         when 'wake_up'
           last_history = current_user.bed_time_histories&.last
-          if last_history&.bed_time.present? && last_history&.wake_up_time.blank?
-            last_history.update!(wake_up_time: system_time)
-          else
-            return render json: { error_message: "you haven't slept yet" },
-                          status: :bad_request
+          unless last_history&.bed_time.present? && last_history&.wake_up_time.blank?
+            raise StandardError, "you haven't slept yet"
           end
+
+          last_history.update!(wake_up_time: system_time)
         else
-          return render json: { error_message: 'type should be exist, accepted value ["bed_time", "wake_up"]' },
-                        status: :bad_request
+          raise StandardError, 'type should be exist, accepted value ["bed_time", "wake_up"]'
         end
 
         render json: { message: "#{params[:type]} has been successfully updated at #{system_time}" }
